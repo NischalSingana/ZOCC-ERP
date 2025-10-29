49f653069bf105783f17785e62221d3b59e431df
Nischal Singana <nischalsingana@Nischals-MacBook-Air.local>
2025-10-30 01:17:45 +0530

    linked components to APP.jsx

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import LiquidEther from './components/LiquidEther.jsx'
+import Login from './components/Login.jsx'
+
+export default function App() {
+  return (
+    <div style={{ position: 'relative', height: '100vh', width: '100%', background: 'transparent' }}>
+      <LiquidEther />
+      <Login />
+    </div>
+  )
+}
+
+

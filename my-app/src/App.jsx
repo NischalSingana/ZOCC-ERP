c430c001e4a5381c2398ce224dfc06e190183338
Nischal Singana <nischalsingana@Nischals-MacBook-Air.local>
2025-10-31 23:42:40 +0530

    fixed css issues

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,16 @@
 import LiquidEther from './components/LiquidEther.jsx'
 import Login from './components/Login.jsx'
+import navLogo from './assets/navLogo.jpg'
+import sacLogo from './assets/sac_logo.png'
+import './App.css'
 
 export default function App() {
   return (
     <div style={{ position: 'relative', height: '100vh', width: '100%', background: 'transparent' }}>
+      <header className="app-header">
+        <img src={navLogo} alt="Title" className="app-logo" />
+        <img src={sacLogo} alt="SAC" className="app-logo-right" />
+      </header>
       <LiquidEther />
       <Login />
     </div>
